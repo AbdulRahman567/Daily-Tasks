5cc45b1d64450ba6b38d4ee5169b4f7eff00774c
AbdulRahman567 <mabdulrahman0508@gmail.com>
2026-06-15 10:37:20 +0500

    Again modified

--- Wednesday/script3.js
+++ Wednesday/script3.js
@@ -3,42 +3,29 @@ const loader = document.getElementById("loader");
 const errorDiv = document.getElementById("error");
 
 loader.style.display = "block";
-
 async function getUsers() {
-
 try {
-
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
-
     const data = await response.json();
-
     loader.style.display = "none";
-
     errorDiv.textContent = "";
-
     data.forEach(user => {
-
         const card = document.createElement("div");
         card.classList.add("card");
-
         card.innerHTML = `
             <h3>${user.name}</h3>
             <p><strong>Email:</strong> ${user.email}</p>
             <p><strong>City:</strong> ${user.address.city}</p>
             <p><strong>Phone:</strong> ${user.phone}</p>
             `;
-
         usersContainer.appendChild(card);
-
         }
     );
 
     } catch (error) {
 
     loader.style.display = "none";
-
     errorDiv.textContent = "Failed to load users. Please try again later.";
-
     console.log("Error:", error);
 
     }
